e6f2d98e262445801d4aa0500754acee42793754
masimacbook <masi19bw@gmail.com>
2019-08-06 21:11:40 +0900

    toggle start set

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,50 @@
 <template>
-  <div id="app">
-    <h1 class="title">Roulette</h1>
-    <div id="roulette">
-      <pie-chart id="pieChart" :chart-data="datacollection" :options="chartOptions"></pie-chart>
+  <div id="container">
+    <div id="navbar">
+      <h1 class="title">Roulette</h1>
     </div>
-    <div id="sideMenu">
-      <p>label 比率</p>
-      <form v-on:submit.prevent>
-        <ul v-for="(item) in items" :key="item.id" class="no-gutters">
-          <li>
-            <input class="textbox" v-model="item.label" placeholder="名称を入力してください" />
-            <input class="textbox" v-model="item.rate" placeholder="比率を入力してください" />
-          </li>
-        </ul>
-        <button @click="fillData()">fillData()</button>
-        <button @click="onAddItems()">onAddItems()</button>
-        <button @click="onDeleteItems()">onDeleteItems()</button>
-        <button @click="onStartRoulette()">Start!</button>
-      </form>
-    </div>
-    <transition name="modal">
-      <div id="overlay" v-show="showContent" v-on:click="closeModal">
-        <div id="content" @click.stop>
-          <p>選ばれたのは「{{picked}}」でした!!!</p>
-          <button v-on:click="closeModal">close</button>
-        </div>
+    <div id="app">
+      <div id="roulette">
+        <pie-chart id="pieChart" :chart-data="datacollection" :options="chartOptions"></pie-chart>
       </div>
-    </transition>
+      <div id="sideMenu">
+        <p id="label">label</p>
+        <p id="rate">比率</p>
+        <form v-on:submit.prevent>
+          <ul v-for="(item) in items" :key="item.id" class="no-gutters">
+            <li>
+              <input
+                type="text"
+                class="textbox label"
+                v-model="item.label"
+                placeholder="名称を入力してください"
+                required
+              />
+              <input
+                type="text"
+                pattern="\d+"
+                class="textbox number"
+                v-model="item.rate"
+                placeholder="比率を入力してください(半角数字)"
+                required
+              />
+            </li>
+          </ul>
+          <button class="btn-flat" @click="fillData()" v-show="!isSet">fillData()</button>
+          <button class="btn-flat" @click="onAddItems()">onAddItems()</button>
+          <button class="btn-flat" @click="onDeleteItems()">onDeleteItems()</button>
+          <button class="btn-flat btn-green" @click="onStartRoulette()" v-show="isSet">Start!</button>
+        </form>
+      </div>
+      <transition name="modal">
+        <div id="overlay" v-show="showContent" v-on:click="closeModal">
+          <div id="content" @click.stop>
+            <p>選ばれたのは「{{picked}}」でした!!!</p>
+            <button v-on:click="closeModal">close</button>
+          </div>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 <script>
@@ -39,6 +57,7 @@ export default {
   },
   mounted() {
     this.fillData();
+    this.isSet = false;
   },
   data() {
     return {
@@ -46,6 +65,7 @@ export default {
       isActive: false,
       showContent: false,
       picked: "",
+      isSet: false,
       items: [
         {
           colorNo: 0,
@@ -96,6 +116,7 @@ export default {
           }
         ]
       };
+      this.isSet = true;
     },
     onAddItems() {
       this.items.push({
@@ -172,6 +193,7 @@ export default {
         pieChart.style.transform = "rotate(-" + stopAngle + "deg)";
         setTimeout(me.openModal, 500);
         me.num = 0;
+        me.isSet = false;
       }, timeOut);
     },
     openModal() {
@@ -185,6 +207,25 @@ export default {
 </script>
 
 <style lang="scss">
+#navbar {
+  background-color: #333;
+  height: 10vh;
+  width: 100vw;
+  color: #fff;
+}
+.btn-flat {
+  display: block;
+  width: 160px;
+  padding: 0.8em;
+  text-align: center;
+  text-decoration: none;
+  color: #333;
+  border-radius: 3px;
+  transition: 0.4s;
+}
+.btn-flat:hover {
+  color: #fff;
+}
 #app {
   max-width: 90%;
   margin: auto;
@@ -201,6 +242,16 @@ ul {
 #sideMenu {
   width: 100%;
   margin: auto;
+  p {
+    display: inline-block;
+    text-align: center;
+  }
+  #label {
+    width: 74%;
+  }
+  #rate {
+    width: 20%;
+  }
 }
 
 #overlay {
@@ -237,13 +288,20 @@ li {
   margin: 10px;
   .textbox {
     border-radius: 5%;
-    width: 49%;
-    height: 30px;
+
+    height: 50px;
+  }
+  .label {
+    width: 75%;
+  }
+
+  .number {
+    width: 20%;
   }
   .textbox:focus {
-    border: 1px solid #da3c41;
+    border: 1px solid #4c3cda;
     outline: none;
-    box-shadow: 0 0 5px 1px rgba(218, 60, 65, 0.5);
+    box-shadow: 0 0 5px 1px rgba(44, 101, 192, 0.5);
   }
 }
 @media (min-width: 768px) {
