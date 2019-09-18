a00182008ba4c367680017589d1a5aa996cf9a13
masimacbook <masi19bw@gmail.com>
2019-09-18 12:42:04 +0900

    Updates

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox/workbox-sw
 
 // Set workbox config
 workbox.setConfig({
-  "debug": true
+  "debug": false
 })
 
 // Start controlling any existing clients as soon as it activates
